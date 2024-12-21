2e352fceab752ed21635590282021cc2fd9eec44
1Dalion61 <arseny.bratukhin@gmail.com>
2024-12-21 08:40:54 +0300

    Django 0.2

--- new_app/views.py
+++ new_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index_page(request):
+    context={
+        "title": 'Курс "Промышленное программирование"',
+        "name": 'Arseny',
+        "count_page": '1'
+    }
+    return render(request, "index.html", context)
